c0768b96e129e455c8c8a6864fd45e92d1b6a930
Ricardo Padilha <ricardospadilha@gmail.com>
2015-07-22 17:33:34 -0700

    Updated to pycrypto 2.6.1

---
+++ app.sh
@@ -0,0 +1,32 @@
+### PYCRYPTO ###
+_build_pycrypto() {
+local VERSION="2.6.1"
+local FOLDER="pycrypto-${VERSION}"
+local FILE="${FOLDER}.tar.gz"
+local URL="https://ftp.dlitz.net/pub/dlitz/crypto/pycrypto/${FILE}"
+local XPYTHON="${HOME}/xtools/python2/${DROBO}"
+local BASE="${PWD}"
+export QEMU_LD_PREFIX="${TOOLCHAIN}/${HOST}/libc"
+
+_download_tgz "${FILE}" "${URL}" "${FOLDER}"
+pushd "target/${FOLDER}"
+sed -e "39ifrom setuptools import setup" -i setup.py
+PKG_CONFIG_PATH="${XPYTHON}/lib/pkgconfig" \
+  LDSHARED="${CC} -shared -Wl,-rpath,/mnt/DroboFS/Share/DroboApps/python2/lib -L${XPYTHON}/lib" \
+  "${XPYTHON}/bin/python" setup.py build_ext \
+  --include-dirs="${XPYTHON}/include" --library-dirs="${XPYTHON}/lib" \
+  --force build --force bdist_egg --dist-dir "${BASE}"
+popd
+}
+
+### BUILD ###
+_build() {
+  _build_pycrypto
+}
+
+_clean() {
+  rm -v -fr *.egg
+  rm -vfr "${DEPS}"
+  rm -vfr "${DEST}"
+  rm -v -fr target/*
+}
