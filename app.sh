aeba40394f390d455c8e5783ac37f0877be975f6
Ricardo Padilha <ricardospadilha@gmail.com>
2015-07-24 13:36:37 -0700

    Updated build scripts

--- app.sh
+++ app.sh
@@ -13,9 +13,10 @@ pushd "target/${FOLDER}"
 sed -e "39ifrom setuptools import setup" -i setup.py
 PKG_CONFIG_PATH="${XPYTHON}/lib/pkgconfig" \
   LDFLAGS="${LDFLAGS:-} -Wl,-rpath,/mnt/DroboFS/Share/DroboApps/python2/lib -L${XPYTHON}/lib" \
-  "${XPYTHON}/bin/python" setup.py build_ext \
-  --include-dirs="${XPYTHON}/include" --library-dirs="${XPYTHON}/lib" \
-  --force build --force bdist_egg --dist-dir "${BASE}"
+  "${XPYTHON}/bin/python" setup.py \
+    build_ext --include-dirs="${XPYTHON}/include" --library-dirs="${XPYTHON}/lib" --force \
+    build --executable="/mnt/DroboFS/Share/DroboApps/python2/bin/python" --force \
+    bdist_egg --dist-dir "${BASE}"
 popd
 }
 
